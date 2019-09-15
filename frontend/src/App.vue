edf203b6b27fa2334fafca9e95910ceec7634d7b
knorrcodes <47395767+knorrcodes@users.noreply.github.com>
2019-09-15 12:07:39 -0500

    Registered 1 additional component and mocked up menu
    in the docs folder, i added some very very very rough draft ideas of what the menu would look like. just to have some sort of visual.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,32 +1,24 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">Home</router-link>|
       <router-link to="/about">About</router-link>
       <div>Hello there</div>
+      <comp1 />
     </div>
-    <router-view/>
   </div>
 </template>
 
+<script>
+import comp1 from "./components/comp1.vue";
+
+export default {
+  name: "app",
+  components: {
+    comp1
+  }
+};
+</script>
+
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
---
+++ frontend/src/components/comp1.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>comp1 stuff</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "comp1"
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
