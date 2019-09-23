8de769469dcbb4cb035f6e45e10a12b7d865be71
knorrcodes <47395767+knorrcodes@users.noreply.github.com>
2019-09-23 12:00:40 -0500

    api updates

--- frontend/src/components/loginScreen.vue
+++ frontend/src/components/loginScreen.vue
@@ -3,8 +3,14 @@
     <div class="id1 container">
       <div class="row justify-content-center">
         <form action>
-          <p class="text-center">Server Login</p>
-          <input id="serverCodeNumber" class="form-control" v-model="serverCode" type="number" />
+          <p class="text-center textFormat">Server Login</p>
+          <input
+            id="serverCodeNumber"
+            class="form-control input1 textFormat"
+            v-model="serverCode"
+            @keydown.enter.prevent="serverCodeCheck"
+            type="number"
+          />
           <br />
           <button
             type="button"
@@ -20,10 +26,12 @@
 
 <script lang="ts">
 import { Component, Vue, Prop } from "vue-property-decorator";
+import ApiClass from "../api";
 
 @Component
 export default class loginScreen extends Vue {
   private serverCode: Number = 0;
+  // private apiData: String = null;
 
   private serverCodeCheck() {
     if (this.serverCode == 478) {
@@ -41,8 +49,17 @@ export default class loginScreen extends Vue {
   justify-content: center;
 }
 .input1 {
+  -webkit-appearance: none;
+  background: rgba(255, 255, 255, 0);
+  text-align: center;
+  border: none;
   display: block;
   margin-left: auto;
   margin-right: auto;
+}
+
+.textFormat {
+  font-family: "Gill Sans", "Gill Sans MT", Calibri, "Trebuchet MS", sans-serif;
+  font-size: 18pt;
 }
 </style>
