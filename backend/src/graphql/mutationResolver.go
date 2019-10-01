cfb2134c6b7888e345a9f5bf0cf00f2b635eb4cb
Lee Keitel <lee@keitel.xyz>
2019-10-01 09:06:41 -0500

    GraphQL mutations check for Manager privilage

---
+++ backend/src/graphql/mutationResolver.go
@@ -0,0 +1,79 @@
+package graphql
+
+//go:generate go run github.com/99designs/gqlgen
+
+import (
+	"context"
+	"errors"
+
+	"koala.pos/src/auth"
+	"koala.pos/src/models"
+	"koala.pos/src/models/stores"
+)
+
+type mutationResolver struct{ *Resolver }
+
+func (r *mutationResolver) CreateProduct(ctx context.Context, input NewProduct) (*models.Product, error) {
+	server := auth.GetServerFromContext(ctx)
+	if server == nil {
+		return nil, errors.New("Failed to check permissions")
+	}
+
+	if !server.Manager {
+		return nil, errors.New("insufficient privilages")
+	}
+
+	storeCollection := stores.GetStoreCollectionFromContext(ctx)
+	if storeCollection == nil {
+		return nil, errors.New("Failed to get storage")
+	}
+
+	product := models.NewProduct(storeCollection.Product)
+
+	// Required fields
+	product.Name = input.Name
+	product.Price = input.Price
+	product.Category = input.Category.ID
+	product.WSCost = input.Wscost
+
+	// Optional fields
+	if input.Desc != nil {
+		product.Desc = *input.Desc
+	}
+	if input.Picture != nil {
+		product.Picture = *input.Picture
+	}
+	if input.NumOfSides != nil {
+		product.NumOfSides = *input.NumOfSides
+	}
+
+	if err := product.Save(); err != nil {
+		return nil, err
+	}
+
+	return product, nil
+}
+
+func (r *mutationResolver) CreateCategory(ctx context.Context, input NewCategory) (*models.Category, error) {
+	server := auth.GetServerFromContext(ctx)
+	if server == nil {
+		return nil, errors.New("Failed to check permissions")
+	}
+
+	if !server.Manager {
+		return nil, errors.New("insufficient privilages")
+	}
+
+	storeCollection := stores.GetStoreCollectionFromContext(ctx)
+	if storeCollection == nil {
+		return nil, errors.New("Failed to get storage")
+	}
+
+	cat := models.NewCategory(storeCollection.Category)
+	cat.Name = input.Name
+	if err := cat.Save(); err != nil {
+		return nil, err
+	}
+
+	return cat, nil
+}
---
+++ backend/src/graphql/queryResolver.go
@@ -0,0 +1,58 @@
+package graphql
+
+//go:generate go run github.com/99designs/gqlgen
+
+import (
+	"context"
+	"errors"
+
+	"koala.pos/src/models"
+	"koala.pos/src/models/stores"
+)
+
+type queryResolver struct{ *Resolver }
+
+func (r *queryResolver) Product(ctx context.Context, id int) (*models.Product, error) {
+	storeCollection := stores.GetStoreCollectionFromContext(ctx)
+	if storeCollection == nil {
+		return nil, errors.New("Failed to get storage")
+	}
+
+	return storeCollection.Product.GetProductByID(id)
+}
+
+func (r *queryResolver) Category(ctx context.Context, id int) (*models.Category, error) {
+	storeCollection := stores.GetStoreCollectionFromContext(ctx)
+	if storeCollection == nil {
+		return nil, errors.New("Failed to get storage")
+	}
+
+	return storeCollection.Category.GetCategoryByID(id)
+}
+
+func (r *queryResolver) Server(ctx context.Context, code int) (*models.Server, error) {
+	storeCollection := stores.GetStoreCollectionFromContext(ctx)
+	if storeCollection == nil {
+		return nil, errors.New("Failed to get storage")
+	}
+
+	return storeCollection.Server.GetByCode(code)
+}
+
+func (r *queryResolver) Products(ctx context.Context) ([]*models.Product, error) {
+	storeCollection := stores.GetStoreCollectionFromContext(ctx)
+	if storeCollection == nil {
+		return nil, errors.New("Failed to get storage")
+	}
+
+	return storeCollection.Product.GetProducts()
+}
+
+func (r *queryResolver) Categories(ctx context.Context) ([]*models.Category, error) {
+	storeCollection := stores.GetStoreCollectionFromContext(ctx)
+	if storeCollection == nil {
+		return nil, errors.New("Failed to get storage")
+	}
+
+	return storeCollection.Category.GetCategories()
+}
